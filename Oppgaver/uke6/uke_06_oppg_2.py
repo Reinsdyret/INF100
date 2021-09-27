4b9ec09414c8b446dcaa1853a15e50e1b7e856cf
reinsdyret <lars.haukland@gmail.com>
2021-09-27 14:25:45 +0200

    helo

--- Oppgaver/uke6/uke_06_oppg_2.py
+++ Oppgaver/uke6/uke_06_oppg_2.py
@@ -1,3 +1,21 @@
 """
-
+Predefined array is of string 0s and 1s making a heart, but rotated 90 degrees against the clock. The wanted output is a heart of 0s
+In the question text x and y is used opposite of what they actually are,
+I will use the same just because
 """
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+#for x in range(len(grid)):
+#    for y in range(len(grid[x])):
+
+for i in range(len(grid)):
+    
